5d2af1e0f6115644b8f0259ae0a866fd5566f9b5
LucienXian <lucienxian97@gmail.com>
2018-06-15 17:10:55 +0800

    implement the class Json except serialize and parser

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Write a simple library for cpp
+
+
+
+该项目用到的C++11新特性：
+
+* enum class
+* alias declarations, using xxx = yyy;
+* explict 
+* std::move
+* nullptr_t
+* runtime_error
+
--- json.cpp
+++ json.cpp
@@ -1,4 +1,5 @@
 #include "json.hpp"
+#include <stdexcept> //for runtime error
 
 namespace lxjson{
 
@@ -6,6 +7,9 @@ using std::string;
 using std::vector;
 using std::map;
 
+static const Json json_null;
+
+//todo:
 static void serialize(std::nullptr_t, string &out){}
 static void serialize(int values, string &out){}
 static void serialize(double values, string &out){}
@@ -13,6 +17,22 @@ static void serialize(bool values, string &out){}
 static void serialize(const string &values, string &out){}
 static void serialize(const Json::array &values, string &out){}
 static void serialize(const Json::object &values, string &out){}
+
+//JsonValue
+double JsonValue::double_value() const               {throw std::runtime_error("not a number!");}
+int JsonValue::int_value() const                     {throw std::runtime_error("not a number!");}
+const string &JsonValue::string_value() const         {throw std::runtime_error("not a string!");}
+bool JsonValue::bool_value() const                   {throw std::runtime_error("not a boolean!");}
+const Json::array &JsonValue::array_value() const    {throw std::runtime_error("not a array!");}
+const Json::object &JsonValue::object_value() const  {throw std::runtime_error("not a object!");}
+
+const Json &JsonValue::operator[](size_t i) const{
+    throw std::runtime_error("not an array");
+}
+
+const Json &JsonValue::operator[](const std::string key) const{
+    throw std::runtime_error("not an object");
+}
 
 template<Json::JsonType tag, typename T>
 class Value: public JsonValue{
@@ -42,6 +62,7 @@ protected:
 
 //Type: number
 class JsonInt final : public Value<Json::JsonType::T_NUMBER, int>{
+public:
     int int_value() const {return m_value;}//cast
     double double_value() const {return m_value;}
     bool equals(const JsonValue * other) const { return m_value == other->double_value(); }
@@ -51,6 +72,7 @@ class JsonInt final : public Value<Json::JsonType::T_NUMBER, int>{
 };
 
 class JsonDouble final : public Value<Json::JsonType::T_NUMBER, double>{
+public:
     int int_value() const {return m_value;}//cast
     double double_value() const {return m_value;}
     bool equals(const JsonValue * other) const { return m_value == other->double_value(); }
@@ -61,6 +83,7 @@ class JsonDouble final : public Value<Json::JsonType::T_NUMBER, double>{
 
 //Type: bool
 class JsonBool final : public Value<Json::JsonType::T_BOOL, bool>{
+public:
     bool bool_value() const {return m_value;}
 
     JsonBool(bool value): Value(value){}
@@ -68,6 +91,7 @@ class JsonBool final : public Value<Json::JsonType::T_BOOL, bool>{
 
 //Type string
 class JsonString final : public Value<Json::JsonType::T_STRING, string>{
+public:
     const string &string_value() const {return m_value;}
 
     JsonString(const string &value): Value(value){}
@@ -75,14 +99,19 @@ class JsonString final : public Value<Json::JsonType::T_STRING, string>{
 };
 
 class JsonArray final : public Value<Json::JsonType::T_ARRAY, Json::array> {
+public:
     const Json::array &array_value() const {return m_value;}
-    const Json &operator[](size_t i) const {return m_value[i];}
+    const Json &operator[](size_t i) const {
+        if (i >= m_value.size()) return json_null;
+        return m_value[i];
+    }
 
     JsonArray(const Json::array &value): Value(value){}
     JsonArray(Json::array &&value): Value(std::move(value)){}
 };
 
 class JsonObject final : public Value<Json::JsonType::T_OBJECT, Json::object> {
+public:
     const Json::object &object_value() const{
         return m_value;
     }
@@ -100,5 +129,115 @@ class JsonNull final : public Value<Json::JsonType::T_NULL, std::nullptr_t> {
 public:
     JsonNull() : Value(nullptr) {}
 };
+
+//Json constructor
+static const std::shared_ptr<JsonValue> obj_null(std::make_shared<JsonNull>());
+static const std::shared_ptr<JsonValue> obj_true(std::make_shared<JsonBool>(true));
+static const std::shared_ptr<JsonValue> obj_false(std::make_shared<JsonBool>(false));
+
+Json::Json() noexcept               : jv_ptr(obj_null){}
+Json::Json(std::nullptr_t) noexcept : jv_ptr(obj_null){}
+Json::Json(double value)            : jv_ptr(std::make_shared<JsonDouble>(value)){}
+Json::Json(int value)               : jv_ptr(std::make_shared<JsonInt>(value)){} 
+Json::Json(bool value)              : jv_ptr(value ? obj_true : obj_false){}
+Json::Json(const std::string& value): jv_ptr(std::make_shared<JsonString>(value)){}
+Json::Json(std::string&& value)     : jv_ptr(std::make_shared<JsonString>(std::move(value))) {}
+Json::Json(const char* value)       : jv_ptr(std::make_shared<JsonString>(value)){}
+Json::Json(const array& value)      : jv_ptr(std::make_shared<JsonArray>(value)){}
+Json::Json(array&& value)           : jv_ptr(std::make_shared<JsonArray>(std::move(value))){}
+Json::Json(const object & value)    : jv_ptr(std::make_shared<JsonObject>(value)){}
+Json::Json(object && value)         : jv_ptr(std::make_shared<JsonObject>(std::move(value))){}
+
+
+Json::Json(const Json& rhs){
+    switch(rhs.type()){
+        case JsonType::T_NULL:
+            jv_ptr = obj_null;
+            break;
+        case JsonType::T_NUMBER:
+            jv_ptr = std::make_shared<JsonDouble>(rhs.double_value());
+            break;
+        case JsonType::T_BOOL:
+            jv_ptr = (rhs.bool_value()? obj_true : obj_false);
+            break;
+        case JsonType::T_STRING:
+            jv_ptr = std::make_shared<JsonString>(rhs.string_value());
+            break;
+        case JsonType::T_ARRAY:
+            jv_ptr = std::make_shared<JsonArray>(rhs.array_value());
+            break;
+        case JsonType::T_OBJECT:
+            jv_ptr = std::make_shared<JsonObject>(rhs.object_value());
+            break;        
+    }
+}
+
+Json::Json(Json&& rhs) noexcept : jv_ptr(std::move(rhs.jv_ptr)) {}
+Json::~Json() {}
+
+Json& Json::operator=(Json rhs){
+    using std::swap;
+    swap(jv_ptr, rhs.jv_ptr);
+}
+
+//comparison
+bool Json::operator==(const Json& rhs) const{
+    if (jv_ptr->type() != rhs.type())
+        return false;
+    return jv_ptr->equals(rhs.jv_ptr.get());
+}
+
+bool Json::operator>(const Json& rhs) const{
+    if (jv_ptr->type() != rhs.type())
+        return false;
+    return rhs.jv_ptr->less(jv_ptr.get());
+}
+
+//check type
+Json::JsonType Json::type()   const {return jv_ptr->type();}
+bool Json::is_null()    const {return type()==JsonType::T_NULL;}
+bool Json::is_number()  const {return type()==JsonType::T_NUMBER;}
+bool Json::is_bool()    const {return type()==JsonType::T_BOOL;}
+bool Json::is_string()  const {return type()==JsonType::T_STRING;}
+bool Json::is_array()   const {return type()==JsonType::T_ARRAY;}
+bool Json::is_object()  const {return type()==JsonType::T_OBJECT;}
+
+//get value
+int Json::int_value()                       const {return jv_ptr->int_value();}
+double Json::double_value()                 const {return jv_ptr->double_value();}
+const string &Json::string_value()          const {return jv_ptr->string_value();}
+bool Json::bool_value()                     const {return jv_ptr->bool_value();}
+const Json::array &Json::array_value()      const {return jv_ptr->array_value();}
+const Json::object &Json::object_value()    const {return jv_ptr->object_value();}
+
+const Json& Json::operator[](size_t i) const{
+    return jv_ptr->operator[](i);
+
+}
+const Json& Json::operator[](const std::string key) const{
+    return jv_ptr->operator[](key);
+}
+
+void Json::serialize(std::string &out) const{
+    jv_ptr->serialize(out);
+}
+
+
+//todo:
+class Parser;
+
+Json Json::parse(const string &in, string &err) {
+    try {
+        Parser p(in);
+        //return p.parse();
+    } catch (std::runtime_error& e) {
+        //std::runtime_error& e;
+        err = e.what();
+        return json_null;
+    }
+
+}
+
+
 
 }
